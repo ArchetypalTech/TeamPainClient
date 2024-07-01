e051ceb642a1236d5b12f762b232941d5e500aa1
JC Nicolas <jeancnicolas@icloud.com>
2024-07-01 17:17:56 +0100

    create for discord

---
+++ src/client.ts
@@ -0,0 +1,22 @@
+import { HoudiniClient } from '$houdini';
+import { createClient } from 'graphql-ws'
+import { subscription } from '$houdini/plugins'
+
+export default new HoudiniClient({
+    url: 'http://127.0.0.1:8080/graphql',
+    plugins: [
+        subscription(() => createClient({
+            url: 'ws://127.0.0.1:8080/graphql/ws'
+        }))
+    ]
+
+    // uncomment this to configure the network call (for things like authentication)
+    // for more information, please visit here: https://www.houdinigraphql.com/guides/authentication
+    // fetchParams({ session }) {
+    //     return {
+    //         headers: {
+    //             Authentication: `Bearer ${session.token}`,
+    //         }
+    //     }
+    // }
+})
---
+++ src/routes/+page.svelte
@@ -0,0 +1,47 @@
+<script lang="ts">
+  import { graphql } from "$houdini";
+  import { browser } from "$app/environment";
+  import type { PageData } from "./$houdini";
+  import { onMount } from "svelte";
+  export let data: PageData;
+
+  $: ({ Nodes } = data);
+
+  $: results = $Nodes.data;
+  $: console.log(results);
+  // let updates;
+  // // will start listening onMount (browser only)
+
+  // const store = graphql(`
+  //   query Test {
+  //     models {
+  //       edges {
+  //         node {
+  //           contractAddress
+  //         }
+  //       }
+  //     }
+  //   }
+  // `);
+  // $: thing = updates ? updates : null;
+  // $: console.log($thing);
+  onMount(() => {
+    const updates = graphql(`
+      query Test {
+        models {
+          edges {
+            node {
+              contractAddress
+            }
+          }
+        }
+      }
+    `);
+    browser && console.log("hello");
+  });
+</script>
+
+<h1>Welcome to SvelteKit</h1>
+<p>
+  Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation
+</p>
